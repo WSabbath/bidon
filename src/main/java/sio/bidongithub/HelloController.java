9ccd9f10bae4d15f73cadce670c7fe5c7334f69f
WSabbath <sabbathwarren@gmail.com>
2023-10-10 17:16:12 +0200

    modif

--- src/main/java/sio/bidongithub/HelloController.java
+++ src/main/java/sio/bidongithub/HelloController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+int dd = 12;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
